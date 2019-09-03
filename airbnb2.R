665aab5eaf32e4677dfcb9da8ad7651f1a48b551
Reet1992 <46289059+Reet1992@users.noreply.github.com>
2019-09-03 14:13:03 +0530

    Add files via upload

---
+++ airbnb2.R
@@ -0,0 +1,33 @@
+setwd("C:/Users/Ted/Desktop/endbegin/test1/Airbnb/")
+
+data <- read.csv("airbnb_modified.csv")
+
+#data$NSP <- as.factor(data$NSP)
+
+set.seed(1234)
+
+#partition data into test and validate
+
+pd <- sample(2,nrow(data),replace = TRUE,prob = c(0.8,0.2))
+train2 <- data[pd==1,]
+validate2 <- data[pd==2,]
+
+
+
+library(partykit)
+
+tree <- ctree(neighbourhood_group~price+availability_365+Hotel_value,data = train2, controls = ctree_control(mincriterion = 0.99,minsplit = 500))
+tree
+plot(tree)  
+
+##predict the tree score again the validate data##
+
+predict(tree,validate1,type = "prob")
+
+
+##misclassification for the train data##
+
+tab <- table(predict(tree),train1$NSP)
+print(tab)
+
+1-sum(diag(tab))/sum(tab)
